ee5e84c7a188e01ff0da59cafc0e0f142de8174d
l.rudolph <leonr502@gmail.com>
2022-07-09 20:11:04 +0200

    natas20

---
+++ natas20.py
@@ -0,0 +1,7 @@
+import natasX
+
+natas = natasX
+
+for i in range(10):
+    print(natas.natasX(20, "eofm3Wsshxc5bwtVnEuGIlr7ivb9KABF", endpoint="index.php",
+                       data={"debug": "", "username": "o9u326p5pt2min81hltemmgdi7"}).text)
